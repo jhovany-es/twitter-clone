31a37e165a004b8b530215d2bd88328a2eeb55c3
jhovany <jhovany.escob@gmail.com>
2022-04-12 19:35:48 -0500

    Updates

---
+++ precache-manifest.29706ce3345218ac2cea20bf48b82546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b170dbcee013306ad7684731024d8abb",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "d1b67b63a0547a72f372",
+    "url": "/twitter-clone/static/css/main.f51f1708.chunk.css"
+  },
+  {
+    "revision": "7d03a0b96f49b38854b7",
+    "url": "/twitter-clone/static/js/2.48798e28.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/twitter-clone/static/js/2.48798e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b67b63a0547a72f372",
+    "url": "/twitter-clone/static/js/main.20585a3c.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
